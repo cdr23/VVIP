c98ff5bff480f1d0b87b434aca739367c2c56756
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-28 03:08:58 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -21,7 +21,7 @@ echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/cdr23/VVIP/ipuk/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
