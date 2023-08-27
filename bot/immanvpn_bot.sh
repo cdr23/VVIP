3fd63f383b320c5566b05869a90af0048895300d
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-08-28 06:04:31 +0700

    Update immanvpn_bot.sh

--- bot/immanvpn_bot.sh
+++ bot/immanvpn_bot.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
 
 [[ ! -f "/etc/IP" ]] && wget -qO- ipv4.icanhazip.com > /etc/IP
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
